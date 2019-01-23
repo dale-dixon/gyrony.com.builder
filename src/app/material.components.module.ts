bccff49ace855fd465d6ac26adb512c11a78536b
Dale Dixon <gyrony@gmail.com>
2019-01-23 10:58:28 -0500

    Configure Angular Material

---
+++ src/app/material.components.module.ts
@@ -0,0 +1,87 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
+
+/**
+ * Uncomment/Comment here to add/remove components
+ */
+const matModules = [
+  // MatAutocompleteModule,
+  // MatBadgeModule,
+  // MatBottomSheetModule,
+  MatButtonModule,
+  // MatButtonToggleModule,
+  // MatCardModule,
+  // MatCheckboxModule,
+  // MatChipsModule,
+  // MatDatepickerModule,
+  MatDialogModule,
+  // MatDividerModule,
+  // MatExpansionModule,
+  MatFormFieldModule,
+  // MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  // MatListModule,
+  // MatMenuModule,
+  // MatPaginatorModule,
+  // MatProgressBarModule,
+  // MatProgressSpinnerModule,
+  // MatRadioModule,
+  // MatRippleModule,
+  // MatSelectModule,
+  // MatSidenavModule,
+  // MatSliderModule,
+  // MatSlideToggleModule,
+  MatSnackBarModule,
+  // MatSortModule,
+  // MatStepperModule,
+  // MatTableModule,
+  // MatTabsModule,
+  // MatToolbarModule,
+  // MatTooltipModule,
+  // MatTreeModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: matModules,
+  exports: matModules
+})
+export class MaterialComponentsModule { }
